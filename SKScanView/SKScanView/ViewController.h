9dad770e9429e6d61926463826a28d8fd714cdd2
ac-hu <hunanlinmac@163.com>
2018-11-07 14:21:58 +0800

    新项目

---
+++ SKScanView/SKScanView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SKScanView
+//
+//  Created by ac-hu on 2018/11/7.
+//  Copyright © 2018年 SK-HU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SKScanView/SKScanView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SKScanView
+//
+//  Created by ac-hu on 2018/11/7.
+//  Copyright © 2018年 SK-HU. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
